7dae69642d1a19859c1dd42aa812fed555419e11
liam-mitchell21 <liam.mitchell@uleth.ca>
2018-01-24 18:19:04 -0700

    Built package learning_tf2, have yet to write launch file

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -117,6 +117,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/liamm/nav-stack/build/gtest/cmake_install.cmake")
+  include("/home/liamm/nav-stack/build/learning_tf2/cmake_install.cmake")
 
 endif()
 

f6e709437a50e8024cd670fb37777c3008c9ea38
liam-mitchell21 <liam.mitchell@uleth.ca>
2018-01-24 22:28:07 -0700

    got rid of all those non-functional demo files, they are now in the appropriate folder

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/liamm/nav-stack/src
-# Build directory: /home/liamm/nav-stack/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(learning_tf2)

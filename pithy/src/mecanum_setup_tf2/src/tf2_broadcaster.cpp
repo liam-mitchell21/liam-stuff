acb66a0b03030d1b36448f7c65ae979f44d62d88
liam-mitchell21 <liam.mitchell@uleth.ca>
2018-01-30 15:21:36 -0700

    prepped some of the tf2 stuff for later

---
+++ pithy/src/mecanum_setup_tf2/src/tf2_broadcaster.cpp
@@ -0,0 +1,36 @@
+#include <ros/ros.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <geometry_msgs/TransformStamped.h>
+
+void(){
+  static tf2_ros::TransformBroadcaster br;
+  geometry_msgs::TransformStamped transformStamped;
+  
+  transformStamped.header.stamp = ros::Time::now();
+  transformStamped.header.frame_id = "world";
+  transformStamped.child_frame_id = "base_link";
+  transformStamped.transform.translation.x = 0.0;
+  transformStamped.transform.translation.y = 0.0;
+  transformStamped.transform.translation.z = 0.0;
+  tf2::Quaternion q;
+  q.setRPY(0, 0, 0);
+  transformStamped.transform.rotation.x = q.x();
+  transformStamped.transform.rotation.y = q.y();
+  transformStamped.transform.rotation.z = q.z();
+  transformStamped.transform.rotation.w = q.w();
+}
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "mecanum_tf2_broadcaster");
+  ros::NodeHandle n;
+
+  ros::Rate r(100);
+
+  while(n.ok()){
+  br.sendTransform(transformStamped);
+  ros::spinOnce();
+  r.sleep();
+  }
+  return 0;
+};
